76b9f432dd200d33ccd38f9c1f1296ce62e37ed9
mind-0023 <rohit.makwana@mindinventory.com>
2021-04-19 16:00:34 +0530

    [Add] Project Setup

---
+++ Practical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical
+//
+//  Created by mind-0023 on 19/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
